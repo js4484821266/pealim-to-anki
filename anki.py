066f53365b98dd689685c328841d82af1e0d2a47
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-02-28 00:09:18 +0900

    unmained, pass pronouns, etc.

--- anki.py
+++ anki.py
@@ -23,44 +23,48 @@ def fusion(soup: BeautifulSoup, id: list[list[str]]) -> str:
     return table
 
 
-if __name__ == '__main__':
-    with open('anki-hebrew.html', 'w', encoding='u8') as f:
-        for wsn in [1230]:
-            r = requests.get('https://www.pealim.com/dict/{}/'
-                             .format(wsn))
-            soup = BeautifulSoup(r.text, 'html.parser')
-            pos = soup.h2.find_next('p').text
-            conjtab = soup.find_all('table', {
-                'class': 'conjugation-table'
-            })
-            id = []
-            if pos.startswith('Verb'):
-                id = [['AP-ms'], ['AP-fs'], ['AP-mp'], ['AP-fp'],
-                      ['PERF-3ms'], ['PERF-3fs'], ['PERF-3p'],
-                      ['PERF-2ms'], ['PERF-2fs'], ['PERF-2mp'],
-                      ['PERF-2fp'], ['PERF-1s'], ['PERF-1p'],
-                      ['IMPF-3ms'], ['IMPF-3mp'],
-                      ['IMPF-3fs', 'IMPF-2ms'], ['IMPF-2fs'],
-                      ['IMPF-2mp'], ['IMPF-3fp', 'IMPF-2fp'],
-                      ['IMPF-1s'], ['IMPF-1p']]
-                if len(conjtab)-1:
-                    id += [['passive-'+j for j in i] for i in id]
-                    soup = BeautifulSoup(
-                        str(conjtab[0])+str(conjtab[1]),
-                        'html.parser')
-                else:
-                    soup = BeautifulSoup(
-                        str(conjtab[0]),
-                        'html.parser')
-                id.insert(0, ['INF-L'])
-            elif pos.startswith('Noun'):
-                id = ['s', 'p', 'sc', 'pc']
-            elif pos.startswith('Adjective'):
-                id = ['ms-a', 'fs-a', 'mp-a', 'fp-a']
-            elif pos.startswith('Preposition'):
-                id = ['P-1s', 'P-2ms', 'P-2fs', 'P-3ms', 'P-3fs',
-                      'P-1p', 'P-2mp', 'P-2fp', 'P-3mp', 'P-3fp']
-            elif pos.startswith('Adverb'):
-                pass
+with open('anki-hebrew.html', 'w', encoding='u8') as f:
+    for wsn in [1230]:
+        r = requests.get('https://www.pealim.com/dict/{}/'
+                         .format(wsn))
+        soup = BeautifulSoup(r.text, 'html.parser')
+        pos = soup.h2.find_next('p').text
+        meaning = soup.find('div', {'class': 'lead'}).text
+        conjtab = soup.find_all('table', {
+            'class': 'conjugation-table'
+        })
+        id = []
+        if pos.startswith('Verb'):
+            id = [['AP-ms'], ['AP-fs'], ['AP-mp'], ['AP-fp'],
+                  ['PERF-3ms'], ['PERF-3fs'], ['PERF-3p'],
+                  ['PERF-2ms'], ['PERF-2fs'], ['PERF-2mp'],
+                  ['PERF-2fp'], ['PERF-1s'], ['PERF-1p'],
+                  ['IMPF-3ms'], ['IMPF-3mp'],
+                  ['IMPF-3fs', 'IMPF-2ms'], ['IMPF-2fs'],
+                  ['IMPF-2mp'], ['IMPF-3fp', 'IMPF-2fp'],
+                  ['IMPF-1s'], ['IMPF-1p']]
+            if len(conjtab)-1:
+                id += [['passive-'+j for j in i] for i in id]
+                soup = BeautifulSoup(
+                    str(conjtab[0])+str(conjtab[1]),
+                    'html.parser')
             else:
-                continue
+                soup = BeautifulSoup(
+                    str(conjtab[0]),
+                    'html.parser')
+            id.insert(0, ['INF-L'])
+        elif pos.startswith('Noun'):
+            id = ['s', 'p', 'sc', 'pc']
+            id = [[i]for i in id]
+        elif pos.startswith('Adjective'):
+            id = ['ms-a', 'fs-a', 'mp-a', 'fp-a']
+            id = [[i]for i in id]
+        elif pos.startswith('Preposition'):
+            id = ['P-1s', 'P-2ms', 'P-2fs', 'P-3ms', 'P-3fs',
+                  'P-1p', 'P-2mp', 'P-2fp', 'P-3mp', 'P-3fp',
+                  'b']
+            id = [[i]for i in id]
+        elif pos.startswith('Pronoun'):
+            continue
+        else:
+            pass#TODO
