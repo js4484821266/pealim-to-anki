361f95e57e5a509960d4455780218fef572fe206
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-04-02 22:20:23 +0900

    minor revision

--- anki.py
+++ anki.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 with open('anki-hebrew.html', 'w', encoding='u8') as f:
-    for wsn in [1]:
+    for wsn in [61]:
         r = requests.get(f'https://www.pealim.com/dict/{wsn}/')
         soup = BeautifulSoup(r.text, 'html.parser')
         pos = soup.h2.find_next('p').text
-        meaning = soup.find('div', {'class': 'lead'})
+        meaning = soup.find('div', {'class': 'lead'}).text
         True
