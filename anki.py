729f638ca373244905dd90631b0583f98f361cc9
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-04-18 05:51:07 +0900

    fix line feed bug

--- anki.py
+++ anki.py
@@ -1,18 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
 with open(f'Hebrew.txt', 'w', encoding='u8') as f:
-    for wsn in range(10,100):
+    for wsn in range(200):
         r = requests.get(f'https://www.pealim.com/dict/{wsn+1}/')
         if r.status_code != 200:
-            print(f'Error: {wsn+1} {r.status_code}')
+            print(f'{r.status_code} {wsn+1}')
             continue
-        print(f'Processing: {wsn+1}')
         soup = BeautifulSoup(r.text, 'html.parser')
         pos = soup.h2.find_next('p').text
-        r = [pos]*2
+        r = ['']*2
         if pos in ['Adverb', 'Conjunction', 'Particle']:
             meaning, w = soup.find_all('div', {'class': 'lead'})
-            r[0] += '<br>' + meaning.text
+            r[0] = '<br>'.join([f'{wsn+1:05d}', pos, meaning.text])
             w = w.contents
             for i in w:
                 i = i.find_all('div')
