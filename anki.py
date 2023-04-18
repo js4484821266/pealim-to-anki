6f5a35aa04d1c7afd315b898335674e4536ade53
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-04-19 02:36:46 +0900

    SN<=1000

--- anki.py
+++ anki.py
@@ -2,10 +2,8 @@ import requests
 from bs4 import BeautifulSoup
 print('--- START ---')
 frame = None
-with open('frame.html', encoding='u8') as f:
-    frame = BeautifulSoup(f, 'html.parser')
 with open(f'Hebrew.txt', 'w', encoding='u8') as f:
-    for wsn in range(1):
+    for wsn in range(1000):
         r = requests.get(f'https://www.pealim.com/dict/{wsn+1}/')
         if r.status_code != 200:
             print(f'ERROR {r.status_code}: {wsn+1}')
@@ -13,15 +11,21 @@ with open(f'Hebrew.txt', 'w', encoding='u8') as f:
         print(wsn+1, end='\r')
         soup = BeautifulSoup(r.text, 'html.parser')
         pos = soup.h2.find_next('p').text
-        h = f'{wsn+1:05d}'+'<br>' + pos
+        for i in ['Verb', 'Noun', 'Adjective', 'Preposition']:
+            if pos .startswith(i):
+                frame = BeautifulSoup(
+                    open(f'frame-{i}.html', encoding='u8'),
+                    'html.parser'
+                )
+                break
+        r = [f'{wsn+1:05d}'+'<br>' + pos+'<br>']*2
         if pos in ['Adverb', 'Conjunction', 'Particle']:
             meaning, w = soup.find_all('div', {'class': 'lead'})
-            r = [h]*2
-            r[0] += '<br>' + meaning.text
+            r[0] += meaning.text
             w = w.contents
             for i in w:
                 i = i.contents
-                r[1] += '<br>'+i[0].text+'<br>'
+                r[1] += i[0].text+'<br>'
                 r[1] += ''.join(map(str, i[1].contents))
             f.write('\t'.join(r)+'\n')
         elif pos == 'Pronoun':
@@ -29,19 +33,16 @@ with open(f'Hebrew.txt', 'w', encoding='u8') as f:
             continue
         else:
             meaning = soup.find('div', {'class': 'lead'})
-            w = soup.find_all('td', {'class': 'conj-td'})
-            w = [i.div for i in w]
+            w = map(lambda x: x.div, soup.find_all('td', {'class': 'conj-td'}))
             for i in w:
-                r = [h]*2
                 try:
-                    r[0] += '<br>'+i.attrs['id']+'<br>' + meaning.text
-                    i = [d for d in i.contents if 'class'not in d.attrs]
-                except Exception as e:
-                    print(wsn+1,e)
-                    continue
-                for j in i:
-                    j = j.contents
-                    r[1] += '<br>' + j[0].text+'<br>'
-                    r[1] += ''.join(map(str, j[1].contents))
-                f.write('\t'.join(r)+'\n')
+                    j = i.attrs['id']
+                    frame.find('div', {'id': j}).extend(
+                        [k for k in i.contents 
+                         if 'class' not in k.attrs])
+                except:
+                    pass
+            r[0] += meaning.text
+            r[1] += str(frame).replace('\n', '')
+            f.write('\t'.join(r)+'\n')
 print('--- END ---')
