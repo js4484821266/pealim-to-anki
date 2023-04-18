a4955c285808199bde21f6c670e0d65271aa032a
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-04-18 22:09:28 +0900

    rearrange files

--- samples/conjugation-table.py
+++ samples/conjugation-table.py
@@ -3,5 +3,5 @@ from bs4 import BeautifulSoup
 with open('htmltest.html','w',encoding='u8')as f:
     r=requests.get('https://www.pealim.com/dict/1/')
     soup=BeautifulSoup(r.text,'html.parser')
-    w=soup.find('table',{'class':'conjugation-table'})
+    w=soup.find('table',{'class':'conjugation-table'}).prettify()
     f.write(str(w))
