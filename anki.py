b3f78da38921e3759eb4e19ade4211df0699cf7f
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-04-18 17:30:57 +0900

    1000<SN<=2000

--- anki.py
+++ anki.py
@@ -1,10 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 with open(f'Hebrew.txt', 'w', encoding='u8') as f:
-    for wsn in range(500,1000):
+    for wsn in range(1000,2000):
         r = requests.get(f'https://www.pealim.com/dict/{wsn+1}/')
         if r.status_code != 200:
-            print(f'Error {r.status_code}: {wsn+1}')
+            print(f'\nERROR {r.status_code}: {wsn+1}')
             continue
         print(f'{wsn+1}',end=' ')
         soup = BeautifulSoup(r.text, 'html.parser')
