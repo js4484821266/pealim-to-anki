dce2fea362ea7e7fd4adc5b83567fc57620db8d4
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-04-18 21:01:52 +0900

    3000<SN<=4000

--- anki.py
+++ anki.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 print('--- START ---')
 with open(f'Hebrew.txt', 'w', encoding='u8') as f:
-    for wsn in range(2195, 3000):
+    for wsn in range(3000, 4000):
         r = requests.get(f'https://www.pealim.com/dict/{wsn+1}/')
         if r.status_code != 200:
             print(f'ERROR {r.status_code}: {wsn+1}')
@@ -34,7 +34,7 @@ with open(f'Hebrew.txt', 'w', encoding='u8') as f:
                     r[0] += '<br>'+i.attrs['id']+'<br>' + meaning.text
                     i = [d for d in i.contents if 'class'not in d.attrs]
                 except Exception as e:
-                    print(wsn+1,' ', e)
+                    print(wsn+1, ' ', e)
                     continue
                 for j in i:
                     j = j.contents
