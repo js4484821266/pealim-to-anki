d729c937e5e3667e491e9f2b59df0fc96f30fb22
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-04-19 12:30:23 +0900

    replace PoS method with ID method

--- anki.py
+++ anki.py
@@ -1,48 +1,61 @@
 import requests
 from bs4 import BeautifulSoup
 print('--- START ---')
-frame = None
+
+# prepare frame-*.html
+frame = {
+    'INF-L': open('frame-Verb.html', encoding='u8').read(),
+    'sc': open('frame-Noun.html', encoding='u8').read(),
+    'ms-a': open('frame-Adjective.html', encoding='u8').read(),
+    'b': open('frame-Preposition.html', encoding='u8').read(),
+    'lead': '<div class="lead"></div>'
+}
+
 with open(f'Hebrew.txt', 'w', encoding='u8') as f:
-    for wsn in range(1000):
+    for wsn in [2682-1]:
         r = requests.get(f'https://www.pealim.com/dict/{wsn+1}/')
         if r.status_code != 200:
             print(f'ERROR {r.status_code}: {wsn+1}')
             continue
         print(wsn+1, end='\r')
         soup = BeautifulSoup(r.text, 'html.parser')
-        pos = soup.h2.find_next('p').text
-        for i in ['Verb', 'Noun', 'Adjective', 'Preposition']:
-            if pos .startswith(i):
-                frame = BeautifulSoup(
-                    open(f'frame-{i}.html', encoding='u8'),
-                    'html.parser'
-                )
+
+        # write header
+        r = [f'{wsn+1:05d}'+'<br>']*2
+
+        # get & pull out meaning
+        r[0] += soup.find('div', {'class': 'lead'}).extract().text
+
+        # decide which frame to use
+        for i in frame:
+            if soup.find('div', {'id': i}):
+                frame = BeautifulSoup(frame[i], 'html.parser')
                 break
-        r = [f'{wsn+1:05d}'+'<br>' + pos+'<br>']*2
-        if pos in ['Adverb', 'Conjunction', 'Particle']:
-            meaning, w = soup.find_all('div', {'class': 'lead'})
-            r[0] += meaning.text
-            w = w.contents
-            for i in w:
-                i = i.contents
-                r[1] += i[0].text+'<br>'
-                r[1] += ''.join(map(str, i[1].contents))
-            f.write('\t'.join(r)+'\n')
-        elif pos == 'Pronoun':
-            print(f'{pos} {wsn+1}')
-            continue
-        else:
-            meaning = soup.find('div', {'class': 'lead'})
-            w = map(lambda x: x.div, soup.find_all('td', {'class': 'conj-td'}))
+
+        # get conjugation table
+        t = soup.find('div', {'id': 'conjugation-table'})
+
+        if t:
+            if t.find('div', {'id': '1s'}):
+                continue
+            w = list(map(lambda x: x.div, t.find_all(
+                'td', {'class': 'conj-td'})))
+            w.insert(0,soup.find('div', {'id': 'b'}))
             for i in w:
                 try:
                     j = i.attrs['id']
                     frame.find('div', {'id': j}).extend(
-                        [k for k in i.contents 
+                        [k for k in i.contents
                          if 'class' not in k.attrs])
                 except:
                     pass
-            r[0] += meaning.text
             r[1] += str(frame).replace('\n', '')
-            f.write('\t'.join(r)+'\n')
+        else:
+            w = soup.find('div', {'class': 'lead'}).contents
+            for i in w:
+                i = i.contents
+                r[1] += i[0].text+'<br>'
+                r[1] += ''.join(map(str, i[1].contents))
+        f.write('\t'.join(r)+'\n')
+
 print('--- END ---')
