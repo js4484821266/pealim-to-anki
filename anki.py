0d6337bc406f65ee624d2026994c6687e2ab807b
js4484821266 <80572363+js4484821266@users.noreply.github.com>
2023-05-18 13:02:17 +0900

    9116<wsn+1<=9117

--- anki.py
+++ anki.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 print('--- START ---')
 
 with open(f'Hebrew.txt', 'w', encoding='u8') as f:
-    for wsn in range(9114,9116):
+    for wsn in range(9116,9117):
         # prepare frame-*.html
         frame = {
             'PERF-3ms': open('frame-Verb.html', encoding='u8').read(),
